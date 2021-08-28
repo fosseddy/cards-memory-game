1790122916c1ad6f24d51eb2090384a0e9d0cf73
Mark Fosseddy <markfosseddy@gmail.com>
2021-08-28 22:13:29 +0500

    add simple card

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-const C_WIDTH: number = 300;
-const C_HEIGHT: number = 200;
+const TILE: number = 20;
+const C_WIDTH: number = TILE * 30;
+const C_HEIGHT: number = TILE * 20;
 
 const canvas: HTMLCanvasElement = document.createElement("canvas");
 const ctx: CanvasRenderingContext2D = (() => {
@@ -15,37 +16,107 @@ const ctx: CanvasRenderingContext2D = (() => {
 canvas.width = C_WIDTH;
 canvas.height = C_HEIGHT;
 canvas.style.border = "1px solid black";
-
 document.body.appendChild(canvas);
 
-let prevTimestamp: number = 0;
-let r = {
-  x: 50,
-  y: 50,
-  w: 50,
-  h: 100
-};
+window.showGrid = false;
+window.flipSpeed = 5;
 
-let a = 1;
+// VEC2
+type Vec2 = {
+  x: number;
+  y: number;
+}
+
+function vec2Create(x: number, y: number): Vec2 {
+  return { x, y };
+}
+
+// CARD
+type Card = {
+  pos: Vec2;
+  center: Vec2;
+  scaleX: number;
+  w: number;
+  h: number;
+  isFlipped: boolean;
+  flipSpeed: number;
+}
+
+function cardCreate(x: number, y: number): Card {
+  const pos = vec2Create(x, y);
+  const w = 2 * TILE;
+  const h = 3 * TILE;
+  const center = vec2Create(x + w / 2, y + h / 2);
+
+  return {
+    pos,
+    center,
+    scaleX: 1,
+    w, h,
+    isFlipped: false,
+    flipSpeed: 0
+  };
+}
+
+function cardDraw(c: Card, ctx: CanvasRenderingContext2D) {
+  ctx.translate(c.center.x, c.center.y);
+  ctx.scale(c.scaleX, 1);
+  ctx.translate(-c.center.x, -c.center.y);
+  ctx.fillStyle = c.scaleX > 0 ? "black" : "red";
+  ctx.fillRect(c.pos.x, c.pos.y, c.w, c.h);
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
+
+function cardUpdate(c: Card, dt: number) {
+  c.scaleX = Number((c.scaleX + (c.flipSpeed * dt)).toFixed(1));
+
+  if (c.scaleX <= -1 || c.scaleX >= 1) {
+    c.flipSpeed = 0;
+  }
+}
+
+// GAME
+const cards: Card[] = [
+  cardCreate(2 * TILE, 2 * TILE),
+  cardCreate(5 * TILE, 2 * TILE),
+];
+
+let prevTimestamp: number = 0;
 
 function gameLoop(timestamp: number) {
-  const dt: number = timestamp - prevTimestamp;
+  const dt: number = (timestamp - prevTimestamp) * 0.001;
+  if (dt <= 0) {
+    requestAnimationFrame(gameLoop);
+    return;
+  }
+  console.assert(dt > 0);
   prevTimestamp = timestamp;
 
+  // UPDATE
+  for (const c of cards) {
+    cardUpdate(c, dt);
+  }
+
+  // DRAW
   ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
+  for (const c of cards) {
+    cardDraw(c, ctx);
+  }
 
-  const xw = r.x + r.w / 2;
-  const yh = r.y + r.h / 2;
+  if (window.showGrid) {
+    for (let x = TILE; x < C_WIDTH; x += TILE) {
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, C_HEIGHT);
+      ctx.stroke();
+    }
 
-  ctx.translate(xw, yh);
-  ctx.scale(a, 1);
-  ctx.translate(-xw, -yh);
-  ctx.fillStyle = a > 0 ? "black" : "red";
-  ctx.fillRect(r.x, r.y, r.w, r.h);
-  ctx.setTransform(1, 0, 0, 1, 0, 0);
-
-  if (a > -1) {
-    a -= 0.5 / dt;
+    for (let y = TILE; y < C_HEIGHT; y += TILE) {
+      ctx.beginPath();
+      ctx.moveTo(0, y);
+      ctx.lineTo(C_WIDTH, y);
+      ctx.stroke();
+    }
   }
 
   requestAnimationFrame(gameLoop);
@@ -53,6 +124,44 @@ function gameLoop(timestamp: number) {
 
 requestAnimationFrame(gameLoop);
 
-canvas.addEventListener("click", () => {
-  a = 1;
+
+function getRelativeMouseCoords(event: MouseEvent): Vec2 {
+  const { clientX, clientY, target } = event;
+  const rect = (target as HTMLElement).getBoundingClientRect();
+
+  return vec2Create(clientX - rect.left, clientY - rect.top);
+}
+
+function clickedInside(c: Card, mouse: Vec2): boolean {
+  return mouse.x >= c.pos.x && mouse.x <= c.pos.x + c.w &&
+         mouse.y >= c.pos.y && mouse.y <= c.pos.y + c.h;
+}
+
+canvas.addEventListener("click", (event: MouseEvent) => {
+  const mouse = getRelativeMouseCoords(event);
+
+  for (const c of cards) {
+    if (clickedInside(c, mouse)) {
+      if (c.isFlipped) {
+        c.flipSpeed = window.flipSpeed;
+      } else {
+        c.flipSpeed = -window.flipSpeed;
+      }
+
+      c.isFlipped = !c.isFlipped;
+
+      console.log(c);
+    }
+  }
+});
+
+window.addEventListener("keypress", (event: KeyboardEvent) => {
+  switch (event.keyCode) {
+    case 103: {
+      window.showGrid = !window.showGrid;
+    } break;
+
+    default:
+      return;
+  }
 });
