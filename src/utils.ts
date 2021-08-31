8ea9babdc9a3be5058bb45cb3d261e10dff15cdb
Mark Fosseddy <markfosseddy@gmail.com>
2021-08-31 21:52:05 +0500

    rename vec2 module to v2

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
-import * as vec2 from "./vec2";
+import * as v2 from "./vec2";
 
 type Shape = {
-  pos: vec2.Vec2;
+  pos: v2.Vec2;
   w: number;
   h: number;
 }
@@ -10,15 +10,15 @@ export function toFixed(float: number, precision: number): number {
   return Number(float.toFixed(precision));
 }
 
-export function getRelativeMouseCoords(event: MouseEvent): vec2.Vec2 {
+export function getRelativeMouseCoords(event: MouseEvent): v2.Vec2 {
   const { clientX, clientY, target } = event;
   const rect = (target as HTMLElement).getBoundingClientRect();
 
-  return vec2.create(clientX - rect.left, clientY - rect.top);
+  return v2.create(clientX - rect.left, clientY - rect.top);
 }
 
 
-export function clickedInside(s: Shape, mouse: vec2.Vec2): boolean {
+export function isInside(s: Shape, mouse: v2.Vec2): boolean {
   return mouse.x >= s.pos.x && mouse.x <= s.pos.x + s.w &&
          mouse.y >= s.pos.y && mouse.y <= s.pos.y + s.h;
 }
